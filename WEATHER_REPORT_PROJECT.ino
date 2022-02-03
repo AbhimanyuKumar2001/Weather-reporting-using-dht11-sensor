92709268ef8bdfcb4bbc53da92aab3e22bd89811
Abhimanyu kumar <64854872+AbhimanyuKumar2001@users.noreply.github.com>
2022-02-03 19:59:09 +0530

    Add files via upload

---
+++ WEATHER_REPORT_PROJECT.ino
@@ -0,0 +1,31 @@
+#include <DHT.h>
+
+
+#define DHT_apin A0 
+ 
+ 
+void setup(){
+ 
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);//Wait before accessing Sensor
+ 
+}
+void loop(){
+  
+ 
+    DHT.read11(DHT_apin);
+    
+    Serial.print("Current humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.temperature); 
+    Serial.println("C  ");
+    
+    delay(5000);
+ 
+ 
+ 
+}
